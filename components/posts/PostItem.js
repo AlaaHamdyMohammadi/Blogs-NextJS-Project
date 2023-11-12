a528420103cf91f4c7a5867937ad4ea8a328e39f
AlaaHamdyMohammadi <alaahamdy2197@gmail.com>
2023-11-12 19:04:49 +0200

    Adding PostItem Component

--- components/posts/PostItem.js
+++ components/posts/PostItem.js
@@ -1,11 +1,29 @@
+import Link from 'next/link';
+import Image from 'next/image';
 import classes from './PostItem.module.css';
 
-function PostItem() {
+function PostItem({post}) {
+    const {title, image, excerpt, date, slug} = post;
+    const formattedDate = new Date(date).toLocaleDateString('en-US', {
+        day: 'numeric',
+        month: 'long',
+        year: 'numeric'
+    })
+    const imagePath = `/images/posts/${slug}/${image}`;
     return (
-        <div>
-            
-        </div>
-    )
+      <li className={classes.post}>
+        <Link>
+          <div className={classes.image}>
+            <Image src={imagePath} alt={title} width={300} height={200} />
+          </div>
+          <div className={classes.content}>
+            <h3>Title: {title}</h3>
+            <time>{formattedDate}</time>
+            <p>The expert: {excerpt}</p>
+          </div>
+        </Link>
+      </li>
+    );
 }
 
 export default PostItem

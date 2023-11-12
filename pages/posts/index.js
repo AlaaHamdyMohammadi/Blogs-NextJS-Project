3e66ebd92cc91b7ace42479594aad3a6f88bfc7d
AlaaHamdyMohammadi <alaahamdy2197@gmail.com>
2023-11-12 19:48:34 +0200

    Adding All Posts

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,9 +1,34 @@
+import AllPosts from "../../components/posts/AllPosts"
+const DUMMY_POSTS = [
+  {
+    title: "NextJs1",
+    image: "getting-started-nextjs.png",
+    excerpt: "NextJs1",
+    date: "2022-02-10",
+    slug: "NextJs1",
+  },
+  {
+    title: "NextJs1",
+    image: "getting-started-nextjs.png",
+    excerpt: "NextJs1",
+    date: "2022-02-10",
+    slug: "NextJs2",
+  },
+  {
+    title: "NextJs1",
+    image: "getting-started-nextjs.png",
+    excerpt: "NextJs1",
+    date: "2022-02-10",
+    slug: "NextJs3",
+  },
+];
+
 function AllPostsPage() {
     return (
-        <div>
-            
-        </div>
-    )
+      <div>
+        <AllPosts posts={DUMMY_POSTS} />
+      </div>
+    );
 }
 
 export default AllPostsPage

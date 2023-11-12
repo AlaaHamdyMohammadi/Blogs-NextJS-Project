f35bec8898f40629f694a2637e1ff58722586dfa
AlaaHamdyMohammadi <alaahamdy2197@gmail.com>
2023-11-12 04:25:02 +0200

    Adding Hero Component

--- components/homePage/Hero.js
+++ components/homePage/Hero.js
@@ -1,8 +1,15 @@
+import Image from 'next/image';
+import classes from './Hero.module.css';
+
 function Hero() {
     return (
-        <div>
-            
+        <section  className={classes.hero}>
+        <div className={classes.image}>
+        <Image src='/images/site/Me.jpeg' alt='My Image' width={300} height={300}/>
         </div>
+        <h1>I'm Alaa</h1>
+        <p>I blog about web development</p>            
+        </section>
     )
 }
 

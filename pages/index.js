03bf8e416237752a9dd343d72c56461c9d3b9a2f
AlaaHamdyMohammadi <alaahamdy2197@gmail.com>
2023-11-14 23:12:34 +0200

    Rendering Posts

--- pages/index.js
+++ pages/index.js
@@ -1,38 +1,49 @@
 import FeaturedPosts from "../components/homePage/FeaturedPosts";
 import Hero from "../components/homePage/Hero";
+import {getFeaturedPosts} from '../lib/posts-util';
 
-const DUMMY_POSTS = [
-  {
-    title: "NextJs1",
-    image: "getting-started-nextjs.png",
-    excerpt: "NextJs1",
-    date: "2022-02-10",
-    slug: "NextJs1",
-  },
-  {
-    title: "NextJs1",
-    image: "getting-started-nextjs.png",
-    excerpt: "NextJs1",
-    date: "2022-02-10",
-    slug: "NextJs2",
-  },
-  {
-    title: "NextJs1",
-    image: "getting-started-nextjs.png",
-    excerpt: "NextJs1",
-    date: "2022-02-10",
-    slug: "NextJs3",
-  },
-];
+// const DUMMY_POSTS = [
+//   {
+//     title: "NextJs1",
+//     image: "getting-started-nextjs.png",
+//     excerpt: "NextJs1",
+//     date: "2022-02-10",
+//     slug: "NextJs1",
+//   },
+//   {
+//     title: "NextJs1",
+//     image: "getting-started-nextjs.png",
+//     excerpt: "NextJs1",
+//     date: "2022-02-10",
+//     slug: "NextJs2",
+//   },
+//   {
+//     title: "NextJs1",
+//     image: "getting-started-nextjs.png",
+//     excerpt: "NextJs1",
+//     date: "2022-02-10",
+//     slug: "NextJs3",
+//   },
+// ];
 
-function HomePage(){
-
+function HomePage(props){
+  const {posts} = props;
     return (
       <>
         <Hero />
-        <FeaturedPosts posts={DUMMY_POSTS} />
+        <FeaturedPosts posts={posts} />
       </>
     );
+}
+
+export function getStaticProps(){
+  const featuredPosts = getFeaturedPosts();
+  return {
+    props: {
+      posts: featuredPosts,
+    },
+    revalidate: 5,
+  };
 }
 
 export default HomePage;
--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,34 +1,47 @@
 import AllPosts from "../../components/posts/AllPosts"
-const DUMMY_POSTS = [
-  {
-    title: "NextJs1",
-    image: "getting-started-nextjs.png",
-    excerpt: "NextJs1",
-    date: "2022-02-10",
-    slug: "NextJs1",
-  },
-  {
-    title: "NextJs1",
-    image: "getting-started-nextjs.png",
-    excerpt: "NextJs1",
-    date: "2022-02-10",
-    slug: "NextJs2",
-  },
-  {
-    title: "NextJs1",
-    image: "getting-started-nextjs.png",
-    excerpt: "NextJs1",
-    date: "2022-02-10",
-    slug: "NextJs3",
-  },
-];
+import { getAllPosts } from "../../lib/posts-util";
 
-function AllPostsPage() {
+// const DUMMY_POSTS = [
+//   {
+//     title: "NextJs1",
+//     image: "getting-started-nextjs.png",
+//     excerpt: "NextJs1",
+//     date: "2022-02-10",
+//     slug: "NextJs1",
+//   },
+//   {
+//     title: "NextJs1",
+//     image: "getting-started-nextjs.png",
+//     excerpt: "NextJs1",
+//     date: "2022-02-10",
+//     slug: "NextJs2",
+//   },
+//   {
+//     title: "NextJs1",
+//     image: "getting-started-nextjs.png",
+//     excerpt: "NextJs1",
+//     date: "2022-02-10",
+//     slug: "NextJs3",
+//   },
+// ];
+
+function AllPostsPage(props) {
+  const {posts} = props;
     return (
       <div>
-        <AllPosts posts={DUMMY_POSTS} />
+        <AllPosts posts={posts} />
       </div>
     );
+}
+
+export function getStaticProps(){
+  const allPosts = getAllPosts();
+  return {
+    props: {
+      posts: allPosts,
+    },
+    revalidate: 5,
+  };
 }
 
 export default AllPostsPage

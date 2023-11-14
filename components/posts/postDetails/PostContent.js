5ded44377e4de87131994551ab60db8573168b3f
AlaaHamdyMohammadi <alaahamdy2197@gmail.com>
2023-11-15 00:18:35 +0200

    Update

--- components/posts/postDetails/PostContent.js
+++ components/posts/postDetails/PostContent.js
@@ -1,6 +1,7 @@
-import ReactMarkdown from 'react-markdown';
-import classes from './PostContent.module.css';
-import PostHeader from './PostHeader';
+import ReactMarkdown from "react-markdown";
+import Image from "next/image";
+import classes from "./PostContent.module.css";
+import PostHeader from "./PostHeader";
 
 // const DUMMY_POST = {
 //     title: "NextJs1",
@@ -10,14 +11,46 @@ import PostHeader from './PostHeader';
 //     slug: "NextJs1",
 //   }
 
-function PostContent({post}) {
-    const imagePath = `/images/posts/${post.slug}/${post.image}`;
-    return (
-      <article className={classes.content}>
-        <PostHeader title={post.title} image={imagePath} />
-        <ReactMarkdown>{post.content}</ReactMarkdown>
-      </article>
-    );
+function PostContent({ post }) {
+  const imagePath = `/images/posts/${post.slug}/${post.image}`;
+  const customComponents = {
+    // img(image) {
+    //   return (
+    //     <Image
+    //       src={`/images/posts/${post.slug}/${image.properties.src}`}
+    //       alt={image.src}
+    //       width={600}
+    //       height={300}
+    //     />
+    //   );
+    // },
+    p(paragraph) {
+      const { node } = paragraph;
+      if (node.children[0].tagName === "img") {
+        const image = node.children[0];
+        return (
+          <div className={classes.image}>
+            <Image
+              src={`/images/posts/${post.slug}/${image.properties.src}`}
+              alt={image.alt}
+              width={600}
+              height={300}
+            />
+          </div>
+        );
+      }
+      return <p>{paragraph.children}</p>
+    },
+  };
+
+  return (
+    <article className={classes.content}>
+      <PostHeader title={post.title} image={imagePath} />
+      <ReactMarkdown components={customComponents}>
+        {post.content}
+      </ReactMarkdown>
+    </article>
+  );
 }
 
-export default PostContent
+export default PostContent;

d0206fb9e2cdb3ed345d920ecbb4b49eccda0f73
AlaaHamdyMohammadi <alaahamdy2197@gmail.com>
2023-11-14 23:38:37 +0200

    Rendering Dynamic Post Page

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -1,9 +1,33 @@
 import PostContent from "../../components/posts/postDetails/postContent"
+import { getPostData, getPostsFiles } from "../../lib/posts-util";
 
-function PostDetailsPage() {
+function PostDetailsPage(props) {
+    const {post} = props;
     return (
-        <PostContent/>
+        <PostContent post={post}/>
     )
+}
+
+export function getStaticProps(context){
+    const {params} = context;
+    const {slug} = params;
+
+    const postData = getPostData(slug);
+    return {
+      props: {
+        post: postData,
+      },
+      revalidate: 5,
+    };
+}
+
+export function getStaticPaths(){
+    const postFilenames = getPostsFiles();
+    const slugs = postFilenames.map((fileName) => fileName.replace(/\.md$/, ""));
+    return{
+        paths: slugs.map((slug) => ({params: {slug}})),
+        fallback: 'blocking',
+    }
 }
 
 export default PostDetailsPage
